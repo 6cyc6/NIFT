ba9c31787b80d36ffb7ad0002a43c47654c5b575
6cyc6 <1412739543@qq.com>
2023-08-21 12:21:28 +0200

    update

--- src/training/losses.py
+++ src/training/losses.py
@@ -2,23 +2,31 @@ import torch
 import torch.nn.functional as F
 
 
-def sdf_net(model_outputs, ground_truth, val=False):
+def occ_sdf_net(model_outputs, ground_truth, val=False):
     loss_dict = dict()
-    label = ground_truth['occ'].squeeze()
+    label_scf = ground_truth['scf'].squeeze()
 
-    dist = F.l1_loss(model_outputs['occ'],label)
-    loss_dict['sdf'] = dist
+    dist = F.l1_loss(model_outputs['scf'], label_scf)
+    loss_dict['scf'] = dist
+
+    label_occ = ground_truth['occ'].squeeze()
+    label_occ = (label_occ + 1) / 2.
+
+    loss_dict['occ'] = -1 * (label_occ * torch.log(model_outputs['occ'] + 1e-5) + (1 - label_occ) * torch.log(
+        1 - model_outputs['occ'] + 1e-5)).mean()
 
     return loss_dict
+
 
 def scf_net(model_outputs, ground_truth, val=False):
     loss_dict = dict()
     label = ground_truth['occ'].squeeze()
 
-    dist = F.l1_loss(model_outputs['occ'],label)
-    loss_dict['scf'] = dist
+    # dist = F.l1_loss(model_outputs['occ'],label)
+    loss_dict['scf'] = F.l1_loss(model_outputs['occ'], label)
 
     return loss_dict
+
 
 def occupancy(model_outputs, ground_truth, val=False):
     loss_dict = dict()
@@ -60,3 +68,14 @@ def semantic(model_outputs, ground_truth, val=False):
         loss_dict['occ'] = -1 * (label * torch.log(model_outputs['occ'].squeeze() + 1e-5) + (1 - label) * torch.log(1 - model_outputs['occ'].squeeze() + 1e-5)).mean()
 
     return loss_dict
+
+
+# source activate lndf
+# cd project/lndf_robot/src/ndf_robot
+#
+# export PYTHONPATH=$PYTHONPATH:`pwd`
+# export LNDF_SOURCE_DIR=/home/kit/anthropomatik/qy7043/project/ndf_robot/src/ndf_robot/data
+# export LNDF_WEIGHT=$PWD/model_weights
+#
+# python training/train_conv_occupancy_net_nift.py --obj_class mug --experiment_name test_lndf_nift --num_epochs 50 \
+# --iters_til_ckpt 1000 --steps_til_summary 100 --batch_size 16 --triplet_loss --multiview_aug --checkpoint_path lnift_test.pth
--- src/training/training.py
+++ src/training/training.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 import torch.distributed as dist
 
 from . import util
+from .losses import occ_sdf_net
 
 
 def average_gradients(model):
@@ -38,6 +39,143 @@ def multiscale_training(model, lr, steps_til_summary, epochs_til_checkpoint, mod
                                   val_loss_fn=val_loss_fn, summary_fn=summary_fn, iters_til_checkpoint=iters_til_checkpoint,
                                   clip_grad=clip_grad, overwrite=overwrite, optimizers=optimizers, batches_per_validation=batches_per_validation,
                                   gpus=gpus, rank=rank, max_steps=max_steps)
+
+
+def train_scf_occ(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_checkpoint, model_dir, loss_fn,
+                  summary_fn=None, iters_til_checkpoint=None, val_dataloader=None, clip_grad=False, val_loss_fn=None,
+                  overwrite=True, optimizers=None, batches_per_validation=10, gpus=1, rank=0, max_steps=None):
+
+    if optimizers is None:
+        optimizers = [torch.optim.Adam(lr=lr, params=model.parameters())]
+
+    if val_dataloader is not None:
+        assert val_loss_fn is not None, "If validation set is passed, have to pass a validation loss_fn!"
+
+    if rank == 0:
+        if os.path.exists(model_dir):
+            if overwrite:
+                shutil.rmtree(model_dir)
+            else:
+                val = input("The model directory %s exists. Overwrite? (y/n)"%model_dir)
+                if val == 'y' or overwrite:
+                    shutil.rmtree(model_dir)
+
+        os.makedirs(model_dir)
+
+        summaries_dir = os.path.join(model_dir, 'summaries')
+        util.cond_mkdir(summaries_dir)
+
+        checkpoints_dir = os.path.join(model_dir, 'checkpoints')
+        util.cond_mkdir(checkpoints_dir)
+
+        writer = SummaryWriter(summaries_dir)
+
+    val_loss_min = 1
+    total_steps = 0
+    with tqdm(total=len(train_dataloader) * epochs) as pbar:
+        train_losses = []
+        for epoch in range(epochs):
+            if not epoch % epochs_til_checkpoint and epoch and rank == 0:
+                torch.save(model.state_dict(),
+                           os.path.join(checkpoints_dir, 'model_epoch_%04d_iter_%06d.pth' % (epoch, total_steps)))
+                np.savetxt(os.path.join(checkpoints_dir, 'train_losses_%04d_iter_%06d.pth' % (epoch, total_steps)),
+                           np.array(train_losses))
+
+            for step, (model_input, gt) in enumerate(train_dataloader):
+                model_input = util.dict_to_gpu(model_input)
+                gt = util.dict_to_gpu(gt)
+
+                start_time = time.time()
+
+                model_output = model(model_input)
+                losses = occ_sdf_net(model_output, gt)
+                # losses = loss_fn(model_output, gt, model_input, model)
+
+                train_loss = 0.
+                for loss_name, loss in losses.items():
+                    single_loss = loss.mean()
+                    if loss_name == "scf":
+                        single_loss *= 10
+
+                    if rank == 0:
+                        writer.add_scalar(loss_name, single_loss, total_steps)
+                    train_loss += single_loss
+
+                train_losses.append(train_loss.item())
+                if rank == 0:
+                    writer.add_scalar("total_train_loss", train_loss, total_steps)
+
+                if not total_steps % steps_til_summary and rank == 0:
+                    torch.save(model.state_dict(),
+                               os.path.join(checkpoints_dir, 'model_current.pth'))
+                    summary_fn(model, model_input, gt, model_output, writer, total_steps)
+
+                for optim in optimizers:
+                    optim.zero_grad()
+                train_loss.backward()
+
+                if gpus > 1:
+                    average_gradients(model)
+
+                if clip_grad:
+                    if isinstance(clip_grad, bool):
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.)
+                    else:
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=clip_grad)
+
+                for optim in optimizers:
+                    optim.step()
+
+                if rank == 0:
+                    pbar.update(1)
+
+                if not total_steps % steps_til_summary and rank == 0:
+                    print("Epoch %d, Total loss %0.6f, iteration time %0.6f" % (epoch, train_loss, time.time() - start_time))
+
+                    if val_dataloader is not None:
+                        print("Running validation set...")
+                        with torch.no_grad():
+                            model.eval()
+                            val_losses = defaultdict(list)
+                            for val_i, (model_input, gt) in enumerate(val_dataloader):
+                                model_input = util.dict_to_gpu(model_input)
+                                gt = util.dict_to_gpu(gt)
+
+                                model_output = model(model_input)
+                                val_loss = val_loss_fn(model_output, gt, val=True)
+
+                                for name, value in val_loss.items():
+                                    val_losses[name].append(value.cpu().numpy())
+
+                                if val_i == batches_per_validation:
+                                    break
+
+                        for loss_name, loss in val_losses.items():
+                            single_loss = np.mean(loss)
+                            summary_fn(model, model_input, gt, model_output, writer, total_steps, 'val_')
+                            writer.add_scalar('val_' + loss_name, single_loss, total_steps)
+
+                        model.train()
+
+                if (iters_til_checkpoint is not None) and (not total_steps % iters_til_checkpoint) and rank == 0:
+                    torch.save(model.state_dict(),
+                               os.path.join(checkpoints_dir, 'model_epoch_%04d_iter_%06d.pth' % (epoch, total_steps)))
+                    np.savetxt(os.path.join(checkpoints_dir, 'train_losses_%04d_iter_%06d.pth' % (epoch, total_steps)),
+                               np.array(train_losses))
+
+                total_steps += 1
+                if max_steps is not None and total_steps==max_steps:
+                    break
+
+            if max_steps is not None and total_steps==max_steps:
+                break
+
+        torch.save(model.state_dict(),
+                   os.path.join(checkpoints_dir, 'model_final.pth'))
+        np.savetxt(os.path.join(checkpoints_dir, 'train_losses_final.txt'),
+                   np.array(train_losses))
+
+        return model, optimizers
 
 
 def train(model, train_dataloader, epochs, lr, steps_til_summary, epochs_til_checkpoint, model_dir, loss_fn,
